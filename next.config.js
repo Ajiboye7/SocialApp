86c06996eda7446cf9d146d18de05480c6d9030d
Ajiboye <ajiboyemuyideen7@gmail.com>
2026-01-09 16:34:15 +0100

    build error

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "img.clerk.com" },
+      { protocol: "https", hostname: "*.ufs.sh" },
+      { protocol: "https", hostname: "utfs.io" },
+      { protocol: "https", hostname: "uploadthing.com" },
+      { protocol: "https", hostname: "f.uploadthing.com" },
+    ],
+  },
+};
+
+module.exports = nextConfig;

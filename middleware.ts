5cae27d513ca2b55e9721b8af378481436cf53cd
Ajiboye <ajiboyemuyideen7@gmail.com>
2026-01-15 17:11:04 +0100

    one more

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,12 @@
-/*import { clerkMiddleware } from '@clerk/nextjs/server';
-
-export default clerkMiddleware();
-
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};*/
-
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Apply middleware to all routes except /api/webhooks, static files, and _next
-    '/((?!api/webhook/clerk|.*\\..*|_next).*)',
-    '/',
+    // Skip Next.js internals and all static files
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
